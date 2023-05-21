b01e15607faf9d74eb69fbb03256775e6cbf1013
Sourav Mondal <souravmondalcode@gmail.com>
2023-05-21 23:37:29 +0600

    name text wave animation

--- app/page.js
+++ app/page.js
@@ -6,19 +6,30 @@ export default function Home() {
   return (
     <main className=" mainClass z-10 flex min-h-screen flex-col items-center justify-between m-5">
 
-      <section className='md:grid grid-cols-2 mt-'>
+      <section className='md:grid grid-cols-2 md:-mt-20'>
 
-        <div>
-          <h1 className='text-4xl  md:text-6xl text-rose-600 font-bold mt-20 '>SOURAV MONDAL</h1>
+        <div className='mx-2 md:ml-24 my-auto'>
+          <div className="nameText ">
+            <h2>SOURAV</h2>
+            <h2>SOURAV</h2>
+          </div>
+          {/* <h1 className='text-4xl  md:text-6xl text-[#05fc64] font-bold '>SOURAV <br></br> MONDAL</h1> */}
           <p className='text-amber-500 text-2xl font-semibold'>Front end Web Developer</p>
-          <p>Hey, I'm Sourav Mondal. A junior front-end MERN stack web developer. And this is my personal portfolio website</p>
+          <p className='text-white'>Hey, I'm Sourav Mondal. A junior front-end MERN stack web developer. And this is my personal portfolio website</p>
         </div>
-        <div className=' flex justify-center items-center w-fit relative'>
-          <Image className=' opacity-40 mx-auto xs:min-w-[320px] sm:min-w-[600px]' src={img2}  height="auto" />
-          <Image className=' absolute  mx-auto my-auto rotate-[30deg] xs:max-w-[200px] sm:max-w-[283px]' src={img1}  height="auto" alt="img" ></Image>
+        <div className=' flex justify-center items-center mx-auto w-fit relative'>
+          <Image className=' opacity-20 mx-auto xs:max-w-[320px] sm:min-w-[600px]' src={img2}  height="auto" />
+          <Image className=' absolute  mx-auto my-auto rotate-[30deg] xs:max-w-[200px] sm:max-w-[283px]' src={img1} height="auto" alt="img" ></Image>
         </div>
 
       </section>
+
+      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
 
 
     </main>
