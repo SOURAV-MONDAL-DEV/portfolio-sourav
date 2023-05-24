b530688837b2315e1ee67514beb06cb9bdb34014
Sourav Mondal <souravmondalcode@gmail.com>
2023-05-24 12:50:02 +0600

    mondal title aded on side

--- app/page.js
+++ app/page.js
@@ -9,10 +9,19 @@ export default function Home() {
       <section className='md:grid grid-cols-2 md:-mt-20'>
 
         <div className='mx-2 md:ml-24 my-auto'>
-          <div className="nameText mx-auto md:ml-0 w-fit ">
-            <h2>SOURAV</h2>
-            <h2>SOURAV</h2>
-          <p className=' text-sky-400 font-semibold text-3xl absolute right-0 bottom-1'>MONDAL</p>
+          <div className='flex justify-center md:justify-start items-center '>
+            <div className="nameText mx-0 md:ml-0 w-fit ">
+              <h2>SOURAV</h2>
+              <h2>SOURAV</h2>
+            </div>
+            <div>
+              <p className=' text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 '>M</p>
+              <p className=' text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 '>O</p>
+              <p className=' text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 '>N</p>
+              <p className=' text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 '>D</p>
+              <p className=' text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 '>A</p>
+              <p className=' text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 '>L</p>
+            </div>
           </div>
           {/* <div className="nameText ">
             <h2 className=''>MONDAL</h2>
@@ -23,7 +32,7 @@ export default function Home() {
           <p className='text-white'>Hey, I'm Sourav Mondal. A junior front-end MERN stack web developer. And this is my personal portfolio website</p>
         </div>
         <div className=' flex justify-center items-center mx-auto w-fit relative'>
-          <Image className=' opacity-20 mx-auto xs:max-w-[320px] sm:min-w-[600px]' src={img2}  height="auto" />
+          <Image className=' opacity-20 mx-auto xs:max-w-[320px] sm:min-w-[600px]' src={img2} height="auto" />
           <Image className=' absolute  mx-auto my-auto rotate-[30deg] xs:max-w-[200px] sm:max-w-[283px]' src={img1} height="auto" alt="img" ></Image>
         </div>
 
