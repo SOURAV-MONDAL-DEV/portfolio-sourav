155098ee656b2ae559fb4d891cbe05da9515b9bf
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-06-23 10:50:48 +0600

    Hero intro section Name and image animated from y

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -8,44 +8,50 @@ import img2 from "/public/images/hex-c.gif";
 
 function Hero() {
   return (
-    <motion.div
-      initial={{ opacity: 0,  scale: 0.5}}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ ease: "linear",delay: 2, duration: 2 }}
-    >
-      <section className="md:grid grid-cols-2 gap-10 md:mt-3 transform scale-90 origin-center">
-        <div className="mx-2  my-auto">
-          <div className="flex justify-center md:justify-start items-center ">
-            <div className="nameText mx-0 md:ml-0 w-fit ">
-              <h2>SOURAV</h2>
-              <h2>SOURAV</h2>
+    <div>
+      <section className="md:grid grid-cols-2 gap-10 md:mt-3 transform scale-[.8] origin-center">
+      <motion.div
+          className="mx-2  my-auto"
+          initial={{ opacity: 0.5, scale: 1, x:-800 }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{ ease: "ease out", delay: 2, duration: 2, type: "spring", stiffness: 100 }}
+        >
+            <div className="flex justify-center md:justify-start items-center  ">
+              <div className="nameText mx-0 md:ml-0 w-fit ">
+                <h2>SOURAV</h2>
+                <h2>SOURAV</h2>
+              </div>
+              <div>
+                <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
+                  M
+                </p>
+                <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
+                  O
+                </p>
+                <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
+                  N
+                </p>
+                <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
+                  D
+                </p>
+                <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
+                  A
+                </p>
+                <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
+                  L
+                </p>
+              </div>
             </div>
-            <div>
-              <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
-                M
-              </p>
-              <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
-                O
-              </p>
-              <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
-                N
-              </p>
-              <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
-                D
-              </p>
-              <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
-                A
-              </p>
-              <p className=" text-sky-400 text-[9px] sm:text-xs md:text-xs leading-[0.5rem] sm:leading-3 md:leading-3 brightness-200 ">
-                L
-              </p>
-            </div>
-          </div>
+            <Intro_pose className="flex justify-center md:justify-start items-center w-fit"></Intro_pose>
+        </motion.div>
 
-          <Intro_pose className="flex justify-center md:justify-start items-center w-fit"></Intro_pose>
 
-          {/* <p className='text-white'>Hey, I'm Sourav Mondal. A junior front-end MERN stack web developer. And this is my personal portfolio website</p> */}
-        </div>
+        <motion.div
+          initial={{ opacity: 0.5, scale: 1, x:800 }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{ ease: "ease out", delay: 2, duration: 2, type: "spring", stiffness: 100 }}
+        
+        >
         <div className=" flex justify-center items-center mx-auto w-fit relative">
           <Image
             className=" opacity-[0.15] mx-auto xs:max-w-[320px] sm:min-w-[445px]"
@@ -59,8 +65,9 @@ function Hero() {
             alt="img"
           ></Image>
         </div>
+        </motion.div>
       </section>
-    </motion.div>
+    </div>
   );
 }
 
