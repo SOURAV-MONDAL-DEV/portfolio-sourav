c716087e2108ea9719a98df852b9b382b0cc32d0
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-07-11 15:07:54 +0600

    Scroll down animation added

--- app/page.js
+++ app/page.js
@@ -32,22 +32,35 @@ export default function Home() {
       <Hero></Hero>
 
       <Link className='text-white  ' href="#about">
-        <button onClick={(e) => handleClick(e)}>About Me</button>
+        <button onClick={(e) => handleClick(e)}>
+          <svg class="arrows">
+            <path class="a1" d="M0 0 L30 32 L60 0"></path>
+            <path class="a2" d="M0 20 L30 52 L60 20"></path>
+            <path class="a3" d="M0 40 L30 72 L60 40"></path>
+          </svg>
+        </button>
       </Link>
 
 
-      <div className='m-10 text-white font-["Lobster"]'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
-      <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
-      <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
-      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
-      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
-      <div className='m-10 text-white'>fhDJFHAUFHU</div>
-      <div className='m-10 text-white font-["Lobster"]'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
-      <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
-      <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
-      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
-      <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
-      <div className='m-10 text-white'>fhDJFHAUFHU</div>
+
+      <div className='mt-[230px]'>
+        <div className='m-10 text-white font-["Lobster"]'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
+        <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
+        <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
+        <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+        <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+        <div className='m-10 text-white'>fhDJFHAUFHU</div>
+        <div className='m-10 text-white font-["Lobster"]'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
+        <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
+        <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
+        <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+        <div className='m-10 text-white'>fhDJFHAUFHUADGUADHFAUDS</div>
+        <div className='m-10 text-white'>fhDJFHAUFHU</div>
+      </div>
+
+
+
+
       <About></About>
       <div className='m-10 text-white font-["Lobster"]'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
       <div className='m-10 text-white'>Hi DJ FHAU FHUA DG UADHFAUDS</div>
