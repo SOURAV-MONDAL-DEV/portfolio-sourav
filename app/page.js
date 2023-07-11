d47dd71e3667b9200a1ba8eccf3afb2684ec52f1
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2023-07-11 16:19:22 +0600

    Scroll down svg angle change

--- app/page.js
+++ app/page.js
@@ -34,9 +34,9 @@ export default function Home() {
       <Link className='text-white  ' href="#about">
         <button onClick={(e) => handleClick(e)}>
           <svg class="arrows">
-            <path class="a1" d="M0 0 L30 32 L60 0"></path>
-            <path class="a2" d="M0 20 L30 52 L60 20"></path>
-            <path class="a3" d="M0 40 L30 72 L60 40"></path>
+            <path class="a1" d="M0 0 L30 25 L60 0"></path>
+            <path class="a2" d="M0 20 L30 45 L60 20"></path>
+            <path class="a3" d="M0 40 L30 65 L60 40"></path>
           </svg>
         </button>
       </Link>
