e225847cddcada87f287cb3d2cdbb9c28be6c15e
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2024-08-19 23:40:24 +0600

    bottom nav width fix in Header2

--- components/Header2/Header2.js
+++ components/Header2/Header2.js
@@ -1,51 +1,58 @@
 "use client";
 import { useEffect, useState } from "react";
-import Link from 'next/link';
-import Image from 'next/image';
-import logoPic from "../../public/animation/wellcomesf.gif"
+import Link from "next/link";
+import Image from "next/image";
+import logoPic from "../../public/animation/wellcomesf.gif";
 import { MdHome } from "react-icons/md";
 import { BsGridFill } from "react-icons/bs";
 import { FcAbout } from "react-icons/fc";
 import { HiMenuAlt2 } from "react-icons/hi";
 
-
 export default function Header2() {
-    const [navbar, setNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        const targetElement = document.getElementById('about');
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      };
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetElement = document.getElementById("about");
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  };
 
-    return (
-        <nav className=" mx-auto rounded-lg fixed w-full bottom-3 z-10 backdrop-blur-lg  origin-center ">
-            <div className=" ">
-                <ul className="flex gap-10 items-center justify-center  shadow backdrop-filter mx-24 my-4 ">
-                                <li className="">
-                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
-                                        <MdHome />
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
-                                        <BsGridFill />
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="#about" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
-                                        <FcAbout onClick={(e) => handleClick(e,"2nddiv")}/>
-                                    </Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
-                                        <HiMenuAlt2 />
-                                    </Link>
-                                </li>
-
-                </ul>
-            </div>
-        </nav>
-    );
-
+  return (
+    <nav className=" mx-auto fixed w-full bottom-3 z-10 transform origin-center ">
+      <ul className="flex gap-10 items-center justify-center bg-white bg-opacity-10 rounded-[16px] shadow backdrop-filter backdrop-blur-sm w-fit mx-auto px-12 py-4">
+        <li>
+          <Link
+            href="/"
+            className="font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md"
+          >
+            <MdHome />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className="font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md"
+          >
+            <BsGridFill />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="#about"
+            className="font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md"
+          >
+            <FcAbout onClick={(e) => handleClick(e, "2nddiv")} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className="font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md"
+          >
+            <HiMenuAlt2 />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
