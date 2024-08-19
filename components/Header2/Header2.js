bf1adc5f6cbad802ecdb175157feda4bcd1f6ed2
SOURAV-MONDAL-DEV <souravmondalcode@gmail.com>
2024-08-19 23:12:46 +0600

    bottom nav icon added in Header2

--- components/Header2/Header2.js
+++ components/Header2/Header2.js
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 import Link from 'next/link';
 import Image from 'next/image';
 import logoPic from "../../public/animation/wellcomesf.gif"
+import { MdHome } from "react-icons/md";
+import { BsGridFill } from "react-icons/bs";
+import { FcAbout } from "react-icons/fc";
+import { HiMenuAlt2 } from "react-icons/hi";
+
 
 export default function Header2() {
     const [navbar, setNavbar] = useState(false);
@@ -14,22 +19,28 @@ export default function Header2() {
       };
 
     return (
-        <nav className=" mx-auto rounded-lg fixed w-full bottom-3 z-10 shadow backdrop-filter backdrop-blur-lg  transform scale-90 origin-center ">
+        <nav className=" mx-auto rounded-lg fixed w-full bottom-3 z-10 backdrop-blur-lg  origin-center ">
             <div className=" ">
-                <ul className="items-center justify-center space-y-8 flex md:space-x-6 md:space-y-0">
+                <ul className="flex gap-10 items-center justify-center  shadow backdrop-filter mx-24 my-4 ">
                                 <li className="">
-                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >Home</Link>
-                                </li>
-                                <li className="">
-                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >Projects</Link>
-                                </li>
-                                <li className="">
-                                    <Link href="#about" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
-                                        <button onClick={(e) => handleClick(e,"2nddiv")}>About</button>
+                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
+                                        <MdHome />
                                     </Link>
                                 </li>
                                 <li className="">
-                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >Contact</Link>
+                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
+                                        <BsGridFill />
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="#about" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
+                                        <FcAbout onClick={(e) => handleClick(e,"2nddiv")}/>
+                                    </Link>
+                                </li>
+                                <li className="">
+                                    <Link href="/" className="  font-semi-bold px-2 py-1 myshadow transition ease-in-out duration-900 hover:scale-110 rounded-md" >
+                                        <HiMenuAlt2 />
+                                    </Link>
                                 </li>
 
                 </ul>
